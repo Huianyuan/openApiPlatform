4ba3a85fd87477ee6fdb3fe86e6f13c93a7fb14a
HuianYuan <452970841@163.com>
2023-04-03 16:38:54 +0800

    更新模拟接口，简化使用SDK

---
+++ imitateInterface/src/main/java/com/example/imitateinterface/src/main/java/com/example/imitateinterface/controller/NameController.java
@@ -0,0 +1,53 @@
+package com.example.imitateinterface.controller;
+
+import com.example.openapiclientsdk.entity.User;
+import com.example.openapiclientsdk.utils.SignUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author :lhy
+ * @description :TODO
+ * @date :2023/03/30 下午 03:11
+ */
+@RestController
+@RequestMapping("/name")
+public class NameController {
+    @GetMapping("/userget")
+    public String getNameByGet(String name) {
+        return "Get 你的名字是" + name;
+    }
+
+    @PostMapping("/userpost")
+    public String getNameByPost(@RequestParam String name) {
+        return "Post 你的名字是" + name;
+    }
+
+    @PostMapping("/user")
+    public String getUserNameByPost(@RequestBody User user, HttpServletRequest request) {
+        String accessKey = request.getHeader("accessKey");
+        String secretKey = request.getHeader("secretKey");
+        String nonce=request.getHeader("nonce");
+        String timestamp=request.getHeader("timestamp");
+        String sign=request.getHeader("sign");
+        String body=request.getHeader("body");
+        //todo 实际从数据库中获取
+        if (!accessKey.equals("ceshi")) {
+            throw new RuntimeException("无权限");
+        }
+
+        // if(Long.parseLong(nonce)<10000){
+        //     throw new RuntimeException("无权限");
+        // }
+        //todo 时间校验，当前时间和调用时间不能超过5分钟
+        // if(timestamp)
+        //todo 从数据库中获取
+        String serverSign = SignUtils.getSign(body, "abcdef");
+        if(!sign.equals(serverSign)){
+            throw new RuntimeException("无权限");
+        }
+        return "post 用户的名字是" + user.getUserName();
+    }
+
+}
