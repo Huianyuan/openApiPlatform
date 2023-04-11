551a7517a871fd5d6d763792937b2b958593f07e
HuianYuan <452970841@163.com>
2023-04-11 14:26:25 +0800

    功能实现
    在线调用接口功能前端方式实现

--- openapi-frontend/src/pages/InterfaceInfo/index.tsx
+++ openapi-frontend/src/pages/InterfaceInfo/index.tsx
@@ -1,11 +1,14 @@
-import { PageContainer } from '@ant-design/pro-components';
+import {PageContainer} from '@ant-design/pro-components';
 import React, {useEffect, useState} from 'react';
 import {Button, Card, Descriptions, Form, Input, message} from "antd";
 
-import { useParams} from "@@/exports";
+import {useParams} from "@@/exports";
 
 // @ts-ignore
-import {getInterfaceInfoByIdUsingGET} from "@/services/openapi-backend/interfaceInfoController";
+import {
+  getInterfaceInfoByIdUsingGET,
+  invokeInterfaceInfoUsingPOST
+} from "@/services/openapi-backend/interfaceInfoController";
 import TextArea from "rc-textarea";
 
 
@@ -17,12 +20,14 @@ const Index: React.FC = () => {
 
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<API.InterfaceInfo>();
-  const params  = useParams();
+  const params = useParams();
+  const [invokeres,setInvokeres]=useState<any>()
+  const [invokeLoading,setinvokeLoading]=useState(false);
 
-  const loadData = async () =>{
-    if (!params.id){
+  const loadData = async () => {
+    if (!params.id) {
       message.error('参数不存在');
-      return ;
+      return;
     }
     setLoading(true);
     try {
@@ -33,27 +38,43 @@ const Index: React.FC = () => {
 
     } catch (error: any) {
 
-      message.error('请求失败,'+error.message);
+      message.error('请求失败,' + error.message);
       return false;
     }
     setLoading(false);
   }
   useEffect(() => {
     loadData();
-  },[])
+  }, [])
 
-  const onFinish = (values: any) => {
-    console.log('Success:', values);
+  const onFinish =async (values: any) => {
+    if(!params.id){
+      message.error('接口不存在');
+      return
+    }
+    setinvokeLoading(true);
+    try {
+      const res=await invokeInterfaceInfoUsingPOST({
+        id:params.id,
+        ...values
+      });
+      setInvokeres(res.data);
+      message.success('请求成功');
+      return true;
+    }catch (error:any){
+      message.error('操作失败，'+error.message);
+    }
+    setinvokeLoading(false);
   };
 
   return (
     <PageContainer title="查看接口文档">
       <Card>
         {
-          data?(
+          data ? (
             <Descriptions title={data.name} column={1}>
               <Descriptions.Item label="描述">{data.description}</Descriptions.Item>
-              <Descriptions.Item label="接口状态">{data.status? '正常': '关闭'}</Descriptions.Item>
+              <Descriptions.Item label="接口状态">{data.status ? '正常' : '关闭'}</Descriptions.Item>
               <Descriptions.Item label="请求地址">{data.url}</Descriptions.Item>
               <Descriptions.Item label="请求方法">{data.method}</Descriptions.Item>
               <Descriptions.Item label="请求参数">{data.requestParams}</Descriptions.Item>
@@ -62,30 +83,33 @@ const Index: React.FC = () => {
               <Descriptions.Item label="创建时间">{data.createTime}</Descriptions.Item>
               <Descriptions.Item label="更新时间">{data.updateTime}</Descriptions.Item>
             </Descriptions>
-          ):(
+          ) : (
             <>接口不存在</>
           )}
       </Card>
-      <Card>
-        <Form
-          name="invoke"
-          layout={"vertical"}
-          onFinish={onFinish}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="请求参数"
-            name="requestParams"
+      <Card title={"在线测试"}>
+          <Form
+            name="invoke"
+            layout={"vertical"}
+            onFinish={onFinish}
+            autoComplete="off"
           >
-            <Input.TextArea />
-          </Form.Item>
+            <Form.Item
+              label="请求参数"
+              name="userRequestParams"
+            >
+              <Input.TextArea/>
+            </Form.Item>
 
-          <Form.Item wrapperCol={{ span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              调用
-            </Button>
-          </Form.Item>
-        </Form>
+            <Form.Item wrapperCol={{span: 16}}>
+              <Button type="primary" htmlType="submit">
+                调用
+              </Button>
+            </Form.Item>
+          </Form>
+      </Card>
+      <Card title={"返回结果"} >
+        {invokeres}
       </Card>
     </PageContainer>
   );
